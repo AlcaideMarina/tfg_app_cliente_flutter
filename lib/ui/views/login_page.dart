f1efeba51de2029bd03ac93020f59ad5ffecb23f
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-01 00:27:41 +0200

    feat: Create login page

---
+++ lib/ui/views/login_page.dart
@@ -0,0 +1,203 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'dart:developer' as developer;
+
+import '../../model/client_model.dart';
+import '../../values/firebase_auth_constants.dart';
+import '../../values/image_routes.dart';
+import '../components/component_text_input.dart';
+import '../components/constants/hn_button.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+bool isButtonEnabled = false;
+String user = '';
+String password = '';
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final double _width = MediaQuery.of(context).size.width;
+    final double _height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+        body: SafeArea(
+          top: false,
+          child: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.fromLTRB(24.0, 0, 24.0, 24.0),
+              width: double.infinity,
+              child: Form(
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 96,
+                    ),
+                    Image.asset(ImageRoutes.getRoute('ic_logo'),
+                        width: 218, height: 287),
+                    const SizedBox(
+                      height: 48,
+                    ),
+                    // TODO: Esto será el usuario, no el correo
+                    HNComponentTextInput(
+                      labelText: 'Correo',
+                      textInputType: TextInputType.emailAddress,
+                      onChange: (text) {
+                        user = text;
+                        setState(() {});
+                      },
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    HNComponentTextInput(
+                      labelText: 'Contraseña',
+                      obscureText: true,
+                      onChange: (text) {
+                        password = text;
+                        setState(() {});
+                      },
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const SizedBox(height: 56)
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        bottomSheet: Container(
+          margin: const EdgeInsets.all(24),
+          child: HNButton(ButtonTypes.redWhiteBoldRoundedButton).getTypedButton(
+              "ACCEDER",
+              null,
+              null,
+              getIsButtonEnabled() ? signIn : null,
+              null),
+        ));
+  }
+
+  bool getIsButtonEnabled() {
+    if (user != '' && password != '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  navigateToMainPage(ClientModel clientModel) {
+    // TODO: Evitar que al dar al botón de atrás, vuelva aquí - investigar
+  }
+
+  // TODO: Esto debería estar en una clase aparte
+  Future<ClientModel?>? getUserInfo() async {
+    String? uid = FirebaseAuth.instance.currentUser?.uid;
+    QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore.instance
+        .collection('client_info')
+        .where('uid', isEqualTo: uid)
+        .get();
+    
+    String id = querySnapshot.docs[0].id;
+    DocumentSnapshot document =
+        await FirebaseFirestore.instance.collection('user_info').doc(id).get();
+
+    if (document.exists) {
+      final Map<String, dynamic>? userInfo = document.data() as Map<String, dynamic>?;
+      if (userInfo != null) {
+        // TODO: hacer un .fromMap
+        return ClientModel(
+          userInfo['cif'], 
+          userInfo['city'], 
+          userInfo['company'], 
+          userInfo['created_by'], 
+          userInfo['deleted'], 
+          userInfo['direction'], 
+          userInfo['email'], 
+          userInfo['has_account'], 
+          userInfo['id'], 
+          userInfo['phone'], 
+          userInfo['postal_code'], 
+          userInfo['province'], 
+          userInfo['uid'], 
+          userInfo['user'], 
+          document.id
+        );
+      } else {
+        return null;
+      }
+    } else {
+        return null;
+      }
+  }
+
+  Future signIn() async {
+    // TODO: añadir un Circular Progress Indicator - que no se pueda quitar
+    // TODO: hacer un componente de pop-up
+    // TODO: Fix - si hay algún error, no se quita el circular progress indicator
+    try {
+      showDialog(
+        context: context, 
+        builder: (_) => const Center(
+          child: CircularProgressIndicator()));
+      developer.log('Empieza la función signInWithEmailAndPassword()', name: 'Login');
+      // TODO: Esto va muy lento - investigar
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: user.trim(), password: password);
+      developer.log('Función signInWithEmailAndPassword() terminada', name: 'Login');
+      developer.log('Empieza la función getUserInfo()', name: 'Login');
+      ClientModel? currentUser = await getUserInfo();
+      developer.log('Función getUserInfo() terminada', name: 'Login');
+      if (currentUser != null) {
+        navigateToMainPage(currentUser);
+      } else {
+        showDialog(context: context, builder: (_) => AlertDialog(
+          title: const Text('Vaya...'),
+          content: const Text('Parece que ha habido un problema. Inténtalo de nuevo más tarde.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('De acuerdo.'),
+              onPressed: () {
+                setState(() {
+                  // TODO: borrar contraseña
+                });
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        ));
+      }
+    } on FirebaseAuthException catch (e) {
+      String errorMessage =
+          '${FirebaseAuthConstants.genericError} Código de error: ${e.code}';
+      if (FirebaseAuthConstants.loginErrors.containsKey(e.code)) {
+        errorMessage = FirebaseAuthConstants.loginErrors[e.code] ?? "";
+      }
+
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                title: const Text('Vaya...'),
+                content: Text(errorMessage),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('De acuerdo.'),
+                    onPressed: () {
+                      setState(() {
+                        // TODO: borrar contraseña
+                      });
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              ));
+    }
+  }
+}
