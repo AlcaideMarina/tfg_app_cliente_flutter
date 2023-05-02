0154a7376166def7dd4199c718830cbb4f86dc79
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-03 01:18:34 +0200

    feat: Add paymentmethod and orderstatus in constants file

--- lib/values/constants.dart
+++ lib/values/constants.dart
@@ -20,3 +20,18 @@ final Map<HomeMenuOptions, String> mapHomeMenuOptions = {
   HomeMenuOptions.newOrder: "Nuevo pedido",
   HomeMenuOptions.settings: "Ajutes",
 };
+
+final Map<String, int> paymentMethod = {
+  "Al contado": 0,
+  "Por recibo": 1,
+  "Transferencia": 2
+};
+
+final Map<String, int> orderStatus = {
+  "Pendiente de precio": 0,
+  "pedido pendiente": 1,
+  "En reparto": 2,
+  "Entregado": 3,
+  "Intento de entrega": 4,
+  "Cancelado": 5
+};
