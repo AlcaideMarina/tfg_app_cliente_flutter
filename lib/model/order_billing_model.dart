595a654ee5edde17553644aa4df5954ced090137
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-03 13:48:49 +0200

    feat: Add more structure billing models

---
+++ lib/model/order_billing_model.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class OrderBillingModel {
+  final String orderId;
+  final Timestamp orderDatetime;
+  final int paymentMethod;
+  final double? totalPrice;
+  final bool paid;
+
+  OrderBillingModel(
+    this.orderId, 
+    this.orderDatetime, 
+    this.paymentMethod, 
+    this.totalPrice, 
+    this.paid
+  );
+  
+}
