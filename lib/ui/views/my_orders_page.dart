12007d5e84abf0c0e3cf138e65f152275e166e00
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-03 03:50:06 +0200

    feat: Create my orders page

---
+++ lib/ui/views/my_orders_page.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:hueveria_nieto_clientes/model/client_model.dart';
+import 'package:hueveria_nieto_clientes/model/db_order_field_data.dart';
+import 'package:hueveria_nieto_clientes/ui/components/component_order.dart';
+import 'package:hueveria_nieto_clientes/firebase/firebase_utils.dart';
+import 'package:hueveria_nieto_clientes/values/utils.dart';
+
+import '../../custom/app_theme.dart';
+import '../../custom/custom_colors.dart';
+import '../../model/order_model.dart';
+import '../components/component_panel.dart';
+import '../components/constants/hn_button.dart';
+import 'dart:developer' as developer;
+
+
+class MyOrdersPage extends StatefulWidget {
+
+  final ClientModel clientModel;
+  final bool fromNewOrderPage;
+
+  const MyOrdersPage(
+    this.clientModel, 
+    this.fromNewOrderPage,
+    {Key? key}) : super(key: key);
+
+  @override
+  State<MyOrdersPage> createState() => _MyOrdersPageState();
+}
+
+class _MyOrdersPageState extends State<MyOrdersPage> {
+  late ClientModel clientModel;
+  bool showProgress = false;
+
+  @override
+  void initState() {
+    super.initState();
+    clientModel = widget.clientModel;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double _width = MediaQuery.of(context).size.width;
+    final double _height = MediaQuery.of(context).size.height;
+    
+    GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return Scaffold(
+        key: _scaffoldKey,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+            toolbarHeight: 56.0,
+            title: const Text(
+              "Ver clientes",
+              style: TextStyle(
+                  color: AppTheme.primary, fontSize: 24),
+            )),
+        body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 56, vertical: 8),
+              child: Column(
+                children: [
+                  HNButton(ButtonTypes.redWhiteBoldRoundedButton)
+                      .getTypedButton(
+                          "Nuevo", null, null, (){}, () {}),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  HNButton(ButtonTypes.grayBlackRoundedButton).getTypedButton(
+                      "Clientes eliminadas",
+                      null,
+                      null,
+                      () {},
+                      () {})
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            StreamBuilder(
+                stream: FirebaseUtils.instance.getOrders(clientModel.doocumentId),
+                builder:
+                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.active) {
+                    if (snapshot.hasData) {
+                      final data = snapshot.data;
+                      final List orderList = data.docs;
+                      if (orderList.isNotEmpty) {
+                        return Expanded(
+                            child: ListView.builder(
+                                shrinkWrap: true,
+                                scrollDirection: Axis.vertical,
+                                itemCount: orderList.length,
+                                itemBuilder: (context, i) {
+                                  final OrderModel order =
+                                      OrderModel.fromMap(orderList[i].data()
+                                          as Map<String, dynamic>);
+                                  return Container(
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 32, vertical: 8),
+                                    child: HNComponentOrders(
+                                        order.orderDatetime,
+                                        order.orderId.toString(),
+                                        Utils().getOrderSummary(DBOrderFieldData.fromMap(order.order)),
+                                        order.totalPrice,
+                                        order.status,
+                                        order.deliveryDni,
+                                        onTap: () {},),
+                                  );
+                                }));
+                      } else {
+                        return Container(
+                            margin: const EdgeInsets.fromLTRB(32, 56, 32, 8),
+                            child: const HNComponentPanel(
+                              title: 'No hay clientes',
+                              text:
+                                  "No hay registro de clientes activos en la base de datos.",
+                            ));
+                      }
+                    } else if (snapshot.hasError) {
+                      return Container(
+                          margin: const EdgeInsets.fromLTRB(32, 56, 32, 8),
+                          child: const HNComponentPanel(
+                            title: 'Ha ocurrido un error',
+                            text:
+                                "Lo sentimos, pero ha habido un error al intentar recuperar los datos. Por favor, inténtelo de nuevo más tarde.",
+                          ));
+                    } else {
+                      return Container(
+                          margin: const EdgeInsets.fromLTRB(32, 56, 32, 8),
+                          child: const HNComponentPanel(
+                            title: 'No hay clientes',
+                            text:
+                                "No hay registro de clientes activos en la base de datos.",
+                          ));
+                    }
+                  }
+                  return const Expanded(
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        color: CustomColors.redPrimaryColor,
+                      ),
+                    ),
+                  );
+                }),
+          ],
+        ));
+  }
+}
