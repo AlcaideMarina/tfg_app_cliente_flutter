a0bbf1681fe9b1af7d49fe1b4eea915196377fbf
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-21 13:58:59 +0200

    refactor: Change order component design

--- lib/ui/components/component_order.dart
+++ lib/ui/components/component_order.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:hueveria_nieto_clientes/values/image_routes.dart';
 import 'package:intl/intl.dart';
 
 import '../../custom/custom_colors.dart';
@@ -50,27 +51,62 @@ class HNComponentOrders extends StatelessWidget {
             children: [
               Expanded(
                 child: Container(
-                  margin: EdgeInsets.only(right: 8),
+                  margin: const EdgeInsets.only(right: 8),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         orderDatetimeStr,
-                        style: TextStyle(fontSize: 10),
+                        style: const TextStyle(fontSize: 11),
                       ),
-                      Text("Pedido: $id"),
+                      const SizedBox(height: 4,),
+                      Row(
+                        children: [
+                          const Text('ID Pedido: ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+                          Text(id.toString(), style: const TextStyle(fontSize: 15))
+                        ],
+                      ),
+                      const SizedBox(height: 12,),
+                      Container(
+                        width: double.infinity,
+                        height: 1,
+                        color: CustomColors.redGrayLightSecondaryColor,
+                        margin: const EdgeInsets.only(left: 12),
+                      ),
+                      const SizedBox(height: 12,),
+                      const Text('Resumen del pedido: ', style: TextStyle(fontWeight: FontWeight.bold),),
                       Text(orderSummary),
-                      Text("Precio: ${price ?? "-"} €"),
+                      const SizedBox(height: 12,),
+                      Container(
+                        width: double.infinity,
+                        height: 1,
+                        color: CustomColors.redGrayLightSecondaryColor,
+                        margin: const EdgeInsets.only(left: 12),
+                      ),
+                      const SizedBox(height: 12,),
+                      Row(
+                        children: [
+                          const Text("Precio: ", style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text("${price ?? "-"} €", style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                        ],
+                      ),
                       Text(Utils().orderStatusIntToString(status) ??
                           "Estado desconocido"),
-                      deliveryDni != null
-                          ? Text("DNI de recogida: $deliveryDni")
-                          : const SizedBox(),
+                      deliveryDni != null ? Row(
+                        children: [
+                          const Text("DNI de recogida: ", style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(deliveryDni!),
+                        ],
+                      ) : const SizedBox(),
                     ],
                   ),
                 ),
               ),
-              const Icon(Icons.arrow_right_alt_outlined)
+              Image.asset(
+                ImageRoutes.getRoute('ic_next_arrow'),
+                width: 16,
+                height: 24,
+              )
             ],
           ),
         ));
